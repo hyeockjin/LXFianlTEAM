270229171c9549a9940571af4a1ca36e0abd85af
khm24 <KHM2407765@gmail.com>
2022-11-12 22:44:50 +0900

    나 김현문 이올시다

--- app/src/main/java/com/lx/project5/EditDogFragment.kt
+++ app/src/main/java/com/lx/project5/EditDogFragment.kt
@@ -6,9 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lx.api.BasicClient
+import com.lx.data.DogListResponse
+import com.lx.data.MemberListResponse
 import com.lx.project5.databinding.FragmentAddDogBinding
 import com.lx.project5.databinding.FragmentEditDogBinding
 import com.lx.project5.databinding.FragmentFirstBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditDogFragment : Fragment() {
     var _binding: FragmentEditDogBinding? = null
@@ -24,9 +30,49 @@ class EditDogFragment : Fragment() {
         binding.educationInput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogEducation}")
         binding.characterInput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogCharacter}")
 
+        binding.editButton.setOnClickListener {
+            editDog()
+        }
 
 
         return binding.root
     }
+    fun editDog(){
+        val dogAge = binding.ageOutput2.getText().toString()
+        val dogCharacter = binding.characterInput2.getText().toString()
+        val dogEducation = binding.educationInput2.getText().toString()
+        val dogBreed = binding.typeOutput2.getText().toString()
+        val dogGender = binding.genderOutput2.getText().toString()
+        val dogImage = "1"
+        val dogName= binding.nameOutput2.getText().toString()
+        val dogNo = AppData.selectedItem?.dogNo.toString()
+        val memberNo= AppData.selectedItem?.memberNo.toString()
+
+
+
+
+        BasicClient.api.postDogUpdate(
+            requestCode = "1001",
+            dogAge = dogAge,
+            dogCharacter = dogCharacter,
+            dogEducation = dogEducation,
+            dogBreed = dogBreed,
+            dogGender = dogGender,
+            dogImage = dogImage,
+            dogName = dogName,
+            dogNo = dogNo,
+            memberNo = memberNo
+        ).enqueue(object : Callback<DogListResponse> {
+            override fun onResponse(call: Call<DogListResponse>, response: Response<DogListResponse>) {
+
+            }
+            override fun onFailure(call: Call<DogListResponse>, t: Throwable) {
+
+                (activity as MainActivity).onFragmentChanged(MainActivity.ScreenItem.ITEMmyPage)
+            }
+        })
+    }
+
+
 
 }
