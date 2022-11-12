f56b74527511db303713cb46111dd0a49ebbd0e5
khm24 <KHM2407765@gmail.com>
2022-11-12 20:49:42 +0900

    나 김현문 이올시다

--- app/src/main/java/com/lx/project5/EditDogFragment.kt
+++ app/src/main/java/com/lx/project5/EditDogFragment.kt
@@ -1,6 +1,7 @@
 package com.lx.project5
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,15 @@ class EditDogFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentEditDogBinding.inflate(inflater, container, false)
+
+        binding.nameOutput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogName}")
+        binding.ageOutput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogAge}")
+        binding.genderOutput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogGender}")
+        binding.typeOutput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogBreed}")
+        binding.educationInput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogEducation}")
+        binding.characterInput2.text = Editable.Factory.getInstance().newEditable("${AppData.selectedItem?.dogCharacter}")
+
+
 
         return binding.root
     }
