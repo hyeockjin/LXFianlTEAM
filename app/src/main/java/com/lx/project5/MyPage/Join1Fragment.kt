258735726d8f197c59ff7ce2fb74e2141d1744c7
Vincent Han <vincent.gurwls123450@gmail.com>
2022-11-19 16:52:08 +0900

    khj_example

---
+++ app/src/main/java/com/lx/project5/MyPage/Join1Fragment.kt
@@ -0,0 +1,27 @@
+package com.lx.project5.MyPage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lx.project5.MainActivity
+import com.lx.project5.databinding.FragmentJoin1Binding
+
+class Join1Fragment : Fragment() {
+    var _binding: FragmentJoin1Binding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentJoin1Binding.inflate(inflater, container, false)
+
+        binding.nextButton1.setOnClickListener {
+            (activity as MainActivity).onFragmentChanged(MainActivity.ScreenItem.ITEMjoin2)
+
+        }
+
+        return binding.root
+    }
+
+
+}
