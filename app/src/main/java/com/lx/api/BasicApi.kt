3bad4ccfbfa862bbb65f0f17266b260790f9b6b8
khm24 <KHM2407765@gmail.com>
2022-11-20 20:37:07 +0900

    중요한건 ??

--- app/src/main/java/com/lx/api/BasicApi.kt
+++ app/src/main/java/com/lx/api/BasicApi.kt
@@ -317,7 +317,7 @@ class BasicClient {
         private const val PROTOCOL = "http"
 
         // 기본 URL
-        private const val BASE_URL = "http://192.168.0.10:8001/"
+        private const val BASE_URL = "http://172.30.1.12:8001/"
 
         // 헤더 속성
         private const val CLIENT_ID = ""
