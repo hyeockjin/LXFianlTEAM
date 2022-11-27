54fa838bc63852284279fa062eedbde675e6ac52
khm24 <KHM2407765@gmail.com>
2022-11-27 17:59:13 +0900

    중요한건 ??

--- app/src/main/java/com/lx/project5/chatting/MemberAdapter.kt
+++ app/src/main/java/com/lx/project5/chatting/MemberAdapter.kt
@@ -2,11 +2,16 @@ package com.lx.project5.chatting
 
 import android.content.Context
 import android.content.Intent
+import android.media.Image
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.lx.project5.appdata.ChatData
 import com.lx.project5.R
 
@@ -29,6 +34,12 @@ class MemberAdapter(private val context: Context, val chatDataList:ArrayList<Cha
         // 데이터 담기, 화면에 데이터 보여주기
         val currentMember = chatDataList[position]
         holder.nameText.text = currentMember.name
+        Log.v("옘병", "${currentMember.memberImage}")
+        currentMember.memberImage?.let{
+            val uri = Uri.parse("http://172.30.1.79:8001/images/${currentMember.memberImage}")
+            Log.v("옘병","${uri}")
+            Glide.with(holder.memberImage).load(uri).into(holder.memberImage)
+        }
 
         holder.itemView.setOnClickListener{
 
@@ -48,5 +59,6 @@ class MemberAdapter(private val context: Context, val chatDataList:ArrayList<Cha
 
     class MemberViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         val nameText: TextView = itemView.findViewById(R.id.name_text)
+        val memberImage: ImageView = itemView.findViewById(R.id.imageButton)
     }
 }
